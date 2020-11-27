67ef672e05fd735db34096d4c8c7bca9c15aa641
PranavKandakurthi <kandakurthipranav@gmail.com>
2020-11-26 19:53:00 -0500

    Added slinghot and catapult

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,10 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+
+        this.slingImage1 = loadImage("sprites/sling1.png");
+        this.slingImage2 = loadImage("sprites/sling2.png");
+        this.slingImage3 = loadImage("sprites/sling3.png");
     }
 
     fly(){
@@ -19,9 +23,26 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(48,22,8);
+            strokeWeight(10);
+
+            if (pointA.x <220){
+                line(pointA.x-20, pointA.y+10, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y+10, pointB.x+20, pointB.y);
+                image(this.slingImage3,pointA.x-20, pointA.y+5);
+
+            }
+            else{
+
+            line(pointA.x+20, pointA.y+7, pointB.x-10, pointB.y);
+            line(pointA.x+20, pointA.y+7, pointB.x+20, pointB.y);
+            image(this.slingImage3,pointA.x+20, pointA.y+5);
+            }
+            
         }
+        image(this.slingImage1,200,20);
+        image(this.slingImage2,170,20);
+        
     }
     
 }
